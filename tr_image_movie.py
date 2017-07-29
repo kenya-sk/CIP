1ffa038eb58c5776b708d5109c26549f378f54d2
MitsukiUsui <mitsuo.com.mu@gmail.com>
2017-07-29 15:47:46 +0900

    changed implementation of tr_image_movie.py
    output loop by filepath_lst
    changed message
    added progress bar

--- tr_image_movie.py
+++ tr_image_movie.py
@@ -6,55 +6,80 @@ import numpy as np
 import cv2
 import argparse
 
+
+def get_lst_by_time(direc, level, timeMax, pageMax):
+	filepath_lst=[]
+	for page in range(1,pageMax+1):
+		for time in range(1,timeMax+1):
+			filepath=direc+"/Pre_Data{0:02d}/t{1:03d}/Pre_Data{0:02d}_t{1:03d}_page_{2:04d}.tif".format(level, time, page)
+			filepath_lst.append(filepath)
+		for _ in range(10):
+			filepath_lst.append(None)
+	return filepath_lst
+
+def get_lst_by_page(direc, level, timeMax, pageMax):
+	filepath_lst=[]
+	for time in range(1,timeMax+1):
+		for page in range(1,pageMax+1):
+			filepath=direc+"/Pre_Data{0:02d}/t{1:03d}/Pre_Data{0:02d}_t{1:03d}_page_{2:04d}.tif".format(level, time, page)
+			filepath_lst.append(filepath)
+		for _ in range(10):
+			filepath_lst.append(None)
+	return filepath_lst
+
 def main(videoFilepath, level=1,axis='time'):
+	
+	#--------------------------------------------------------------------------------
+	# load config.txt
+	#--------------------------------------------------------------------------------
 	try:
 		with open("config.txt", 'r') as f:
 			direc=f.readline().strip()
 	except FileNotFoundError:
-		print("NotFound: config.txt")
+		print("Not Found: config.txt")
 		print("\tPlease specify file directory in ./config.txt")
 		sys.exit(1)
 	
-	direc+="/Pre_Data{0:02d}".format(level)
-	inputFilepath=direc+"/input.csv"
+	#--------------------------------------------------------------------------------
+	# load input.csv to get timeMax and pageMax
+	#--------------------------------------------------------------------------------
+	inputFilepath=direc+"/Pre_Data{0:02d}/input.csv".format(level)
 	with open(inputFilepath, 'r') as f:
 		f.readline()
 		timeMax=int(f.readline().strip())
 		pageMax=int(f.readline().strip())
-	print(timeMax, pageMax)
-	print(videoFilepath)
 
-	
+	#--------------------------------------------------------------------------------
+	# video output configuration
+	#--------------------------------------------------------------------------------
 	fourcc = int(cv2.VideoWriter_fourcc(*'avc1'))
 	video = cv2.VideoWriter(videoFilepath,fourcc,10.0,(480,480))
 	waitImg = np.zeros((480,480,3),np.uint8)#image for waiting
-	if axis == 'time':
-		time_scale(timeMax,pageMax,level,direc,video,waitImg)
+
+
+	#--------------------------------------------------------------------------------
+	# output .mp4 with designated order 
+	#--------------------------------------------------------------------------------
+	if axis=="time":
+		filepath_lst=get_lst_by_time(direc, level, timeMax, pageMax)
+	elif axis=="page":
+		filepath_lst=get_lst_by_page(direc, level, timeMax, pageMax)
 	else:
-		depth_scale(timeMax,pageMax,level,direc,video,waitImg)
+		print("Bad Axis Error: {}".format(axis))
+		sys.exit(1)
 
-
-def depth_scale(timeMax,pageMax,level,direc,video,waitImg):
-	for time in range(1,timeMax+1):
-		for page in range(1,pageMax+1):
-			filepath=direc+"/t{0:03d}/Pre_Data{1:02d}_t{0:03d}_page_{2:04d}.tif".format(time, level, page)
-			img = cv2.imread(filepath)
+	print("level: {}, axis: {}".format(level, axis))
+	for i,filepath in enumerate(filepath_lst):
+		if i % 100==0:
+			print('.',end='', flush=True)
+		if filepath is not None:
+			img=cv2.imread(filepath)
 			video.write(img)
-		for _ in range(10):
+		else:
 			video.write(waitImg)
-	video.release()
+	print()
+	print("DONE: {}".format(videoFilepath))
 
-
-def time_scale(timeMax,pageMax,level,direc,video,waitImg):
-	for page in range(1,pageMax+1):
-		for time in range(1,timeMax+1):
-			filepath=direc+"/t{0:03d}/Pre_Data{1:02d}_t{0:03d}_page_{2:04d}.tif".format(time, level, page)
-			img = cv2.imread(filepath)
-			video.write(img)
-		for _ in range(10):
-			video.write(waitImg)
-	video.release()
-	
 	
 def make_parse():
 	parser = argparse.ArgumentParser(prog='tr_image_movie.py',
