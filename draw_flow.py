52155e02c5dd7480235e7873fdd7358d84658712
kenya-sk <kenya0sakka@gmail.com>
2017-09-07 10:38:46 +0900

    modify config function

--- draw_flow.py
+++ draw_flow.py
@@ -6,26 +6,12 @@ import time
 import numpy as np
 import cv2
 from configparser import ConfigParser
-import util
+import ciputil
 
-BASEDIR = None
 LEVEL = None
 TIME_MAX = None
 PAGE_MAX = None
 OUTPUT_VIDEO = None
-
-
-def get_level_configuration(level):
-    inputFilepath = BASEDIR + "/Pre_Data{0:02d}/input.csv".format(level)
-    try:
-        with open(inputFilepath,"r") as f:
-            f.readline()
-            timeMax = int(f.readline().strip())
-            pageMax = int(f.readline().strip())
-    except FileNotFoundError:
-        print("Not Found: {}".format(configFilepath))
-        sys.exit(1)
-    return timeMax, pageMax
 
 
 def output_video_with_flow(configFilepath):
@@ -61,15 +47,12 @@ def output_video_with_flow(configFilepath):
                         criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
     for page in range(pageFirst, pageLast + 1):
-        prevImg = util.get_image(BASEDIR + \
-                        "/Pre_Data{0:02d}/t001/Pre_Data{0:02d}_t001_page_{1:04d}.tif".format(LEVEL, page))
+        prevImg = ciputil.get_image(LEVEL, 1, page)
         prevGray = cv2.cvtColor(prevImg, cv2.COLOR_BGR2GRAY)
         prevFeature = cv2.goodFeaturesToTrack(prevGray, mask=None, **feature_params)
         for time in range(1, TIME_MAX + 1):
-            filepath = BASEDIR + \
-                "/Pre_Data{0:02d}/t{1:03d}/Pre_Data{0:02d}_t{1:03d}_page_{2:04d}.tif".format(LEVEL, time, page)
             mask = np.zeros((480, 480, 3), np.uint8)
-            nextImg = util.get_image(filepath)
+            nextImg = ciputil.get_image(LEVEL, time, page)
             nextGray = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
             nextFeature, status, err = cv2.calcOpticalFlowPyrLK(prevGray, nextGray, prevFeature, None, **lk_params)
             prevGood = prevFeature[status == 1]
@@ -92,15 +75,13 @@ def output_video_with_flow(configFilepath):
 
 
 def main():
-    global BASEDIR
     global LEVEL
     global TIME_MAX
     global PAGE_MAX
     global OUTPUT_VIDEO
 
     configFilepath = "./config/config.ini"
-    BASEDIR, LEVEL, OUTPUT_VIDEO = util.read_config(configFilepath)
-    TIME_MAX, PAGE_MAX = get_level_configuration(LEVEL) 
+    LEVEL, TIME_MAX, PAGE_MAX, OUTPUT_VIDEO = ciputil.read_config(configFilepath)
     
     if OUTPUT_VIDEO:
         output_video_with_flow(configFilepath)
