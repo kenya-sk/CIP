e5870910bc2e3b28ccb36f131ea466e88e9f219d
kenya-sk <kenya0sakka@gmail.com>
2017-09-09 18:39:55 +0900

    modify

--- stabilize.py
+++ stabilize.py
@@ -21,25 +21,28 @@ def calc_fix_direction():
     calculate fix direction for at each time point.
     fixDirection_arr[time] = (fixDirectionX, fixDirectionY, fixDirectionZ)
     """
-
-    def calc_flow(prevImg, nextImg, prevFeature, lk_params):
+    def get_feature(prevImg, nextImg, prevFeature):
         assert prevImg.shape == nextImg.shape
+        #parameter of Lucas-Kanade method
+        lk_params = dict(winSize = (20, 20),
+                            maxLevel = 5,
+                            criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
         if len(prevImg.shape) == 3:
             prevImg = cv2.cvtColor(prevImg, cv2.COLOR_BGR2GRAY)
             nextImg = cv2.cvtColor(nextImg, cv2.COLOR_BGR2GRAY)
             nextFeature, status, err = cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevFeature, None, **lk_params)
             prevGood = prevFeature[status == 1]
             nextGood = nextFeature[status == 1]
-            
-            flow = [[],[],[0,0]]
-            for nextPoint,prevPoint in zip(nextGood, prevGood):
-                prevX, prevY = prevPoint.ravel()
-                nextX, nextY = nextPoint.ravel()
-                flow[0].append(nextX - prevX)
-                flow[1].append(nextY - prevY)
-            prevFeature = nextGood.reshape(-1, 1, 2)
-        return flow, prevFeature
+        return prevGood, nextGood
 
+    def calc_flow(prevGood, nextGood):
+        flow = np.zeros((3,nextGood.shape[0]))
+        for i, (nextPoint, prevPoint) in enumerate(zip(nextGood, prevGood)):
+            prevX, prevY = prevPoint.ravel()
+            nextX, nextY = nextPoint.ravel() 
+            flow[0][i] = nextX - prevX
+            flow[1][i] = nextY - prevY
+        return flow
 
     def calc_movement(flow, criterion="mode"):
         if criterion == "mode":
@@ -52,9 +55,7 @@ def calc_fix_direction():
                 modeY = st.mode(flow[1])
                 modeZ = st.mode(flow[2])
             except st.StatisticsError:
-                modeX = 0
-                modeY = 0
-                modeZ = 0
+                modeX, modeY, modeZ = 0, 0, 0
             movement = np.array([modeX, modeY, modeZ])
         elif criterion == "median":
             try:
@@ -62,49 +63,44 @@ def calc_fix_direction():
                 medianY = st.median(flow[1])
                 medianZ = st.median(flow[2])
             except st.StatisticsError:
-                medianX = 0
-                medianY = 0
-                medianZ = 0
+                medianX, medianY, medianZ = 0, 0, 0
             movement = np.array([medianX, medianY, medianZ])
         elif criterion == "mean":
             try:
-                meanX = sum(flow[0])/len(flow[0])
-                meanY = sum(flow[1])/len(flow[1])
-                meanZ = sum(flow[2])/len(flow[2])
+                meanX, meanY, meanZ = np.mean(flow,axis=1)
             except ZeroDivisionError:
-                meanX = 0
-                meanY = 0
-                meanZ = 0
+                meanX, meanY, meanZ = 0, 0, 0
             movement = np.array([meanX, meanY, meanZ])
+        else:
+            print("Error: criterion")
+            sys.exit(1)
         return movement
 
-    #-------------------------------------------------
-    #sparse optical flow parameter
-    #-------------------------------------------------
-    #corner detection parameter of Shi-Tomasi
+
+    fixDirection_arr = np.zeros((PAGE_MAX,TIME_MAX + 1, 3))  # +1 to adjust to 1 origin of time
+    allPage_fixDirection_arr = np.zeros((TIME_MAX + 1, 3))
+    
     feature_params = dict(maxCorners = 200,
                             qualityLevel = 0.001,
                             minDistance = 10,
                             blockSize = 5)
-    #parameter of Lucas-Kanade method
-    lk_params = dict(winSize = (20, 20),
-                        maxLevel = 5,
-                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
-    fixDirection_arr = np.zeros((TIME_MAX + 1, 3))  # +1 to adjust to 1 origin of time
-    prevImg = ciputil.get_image(level=LEVEL, time=1, page=1)
-    prevGray = cv2.cvtColor(prevImg, cv2.COLOR_BGR2GRAY)
-    prevFeature = cv2.goodFeaturesToTrack(prevGray, mask=None, **feature_params)
-    for time in range(2, TIME_MAX + 1):
-        nextImg = ciputil.get_image(level=LEVEL, time=time, page=1)
-        flow, prevFeature = calc_flow(prevImg, nextImg, prevFeature, lk_params)
-        movement = calc_movement(flow, "mode")
-        for i in range(3):
-            fixDirection_arr[time -1][i] = fixDirection_arr[time - 2][i] + movement[i]
-        prevImg = nextImg
+    
+    for page in range(1, PAGE_MAX + 1):
+        prevImg = ciputil.get_image(level=LEVEL, time=1, page=page)
+        prevGray = cv2.cvtColor(prevImg, cv2.COLOR_BGR2GRAY)
+        prevFeature = cv2.goodFeaturesToTrack(prevGray, mask=None, **feature_params)
+        for time in range(2, TIME_MAX + 1):
+            nextImg = ciputil.get_image(level=LEVEL, time=time, page=page)
+            prevGood, nextGood = get_feature(prevImg, nextImg, prevFeature)
+            flow = calc_flow(prevGood, nextGood)
+            movement = calc_movement(flow, "mean")
+            prevFeature = nextGood.reshape(-1, 1, 2)
+            for i in range(3):
+                fixDirection_arr[page-1][time][i] = fixDirection_arr[page-1][time - 1][i] + movement[i]
+            prevImg = nextImg
 
     return fixDirection_arr
-
+            
 
 def output_fix_direction(fixDirection_arr, outputFilepath):
     """
@@ -141,9 +137,9 @@ def output_stabilized_video(fixDirection_arr, configFilepath):
             height, width = img.shape[:2]
             fixHeight, fixWidth = fixImg.shape[:2]
 
-            fixDirectionX = int(fixDirection_arr[time][0])
-            fixDirectionY = int(fixDirection_arr[time][1])
-            fixDirectionZ = int(fixDirection_arr[time][2])
+            fixDirectionX = int(fixDirection_arr[page-1][time][0])
+            fixDirectionY = int(fixDirection_arr[page-1][time][1])
+            fixDirectionZ = int(fixDirection_arr[page-1][time][2])
 
             fixImg[int((fixHeight - height) / 2) - fixDirectionY:
                    int(height + (fixHeight - height) / 2) - fixDirectionY,
