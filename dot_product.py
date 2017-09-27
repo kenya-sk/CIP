b0c62105d886e84860e1913970ef9a32849999d4
takebiolac <takefumi.biology@gmail.com>
2017-09-28 00:48:27 +0900

    resolve requested changes

--- dot_product.py
+++ dot_product.py
@@ -32,6 +32,9 @@ def calc_dot_product(cmlFlow_arr, windowSize, flowThreshold):
     return dotProduct_arr[time][960][960], 1 origin time
     """
 
+    assert windowSize%2 == 1
+    assert windowSize >= 3
+
     def dot_product(flow, windowSize, flowThreshold):
         """
         return matrix[960][960] of minimum dot product for each pixel
@@ -39,41 +42,34 @@ def calc_dot_product(cmlFlow_arr, windowSize, flowThreshold):
 
         assert flow.shape[0] == flow.shape[1]
         width = flow.shape[0]
-
         margin = int(windowSize/2)
-        widthMargin = width + margin*2 #two direction +, -
+        widthMargin = margin + width + margin #two direction +, -
 
-        neighborNum = ((margin + 1)*(margin*2 + 1) - 1)*2 #((num of xShift) * (num of yShift) - (0,0)) * (reverse direction)
-        dotProductNeighbor = np.zeros ((neighborNum, widthMargin, widthMargin))
-        
         flow[np.sum(flow*flow, axis=2) < flowThreshold]=[0,0]
-
         flowMargin = np.zeros((widthMargin, widthMargin, 2))
         flowMargin[margin:-margin, margin:-margin] = flow
-
+        
+        neighborNum = ((margin + 1)*(margin*2 + 1) - 1)*2 #((num of xShift) * (num of yShift) - (0,0)) * (reverse direction)
+        dotProductNeighbor = np.zeros ((neighborNum, widthMargin, widthMargin))
         shiftIterator = 0
         for xShift in range(margin + 1):
             for yShift in range(-margin, margin + 1):
                 if (xShift != 0 or yShift != 0):
                     shifted = np.zeros((widthMargin, widthMargin, 2))
-                    shifted[margin + xShift : width + margin + xShift, margin + yShift : width + margin + yShift] = flow
+                    shifted[margin + xShift : margin + width + xShift, margin + yShift : margin + width + yShift] = flow
                     dotProductNeighbor[shiftIterator] = np.sum(flowMargin * shifted, axis=2)
                     dotProductNeighbor[shiftIterator + 1][margin:-margin, margin:-margin] \
-                        = dotProductNeighbor[shiftIterator][margin + xShift : width + margin + xShift, margin + yShift : width + margin + yShift]
+                        = dotProductNeighbor[shiftIterator][margin + xShift : margin + width + xShift, margin + yShift : margin + width + yShift]
                     shiftIterator += 2
-
         assert shiftIterator == neighborNum
 
         dotProduct = np.min(dotProductNeighbor, axis=0)
-
         return dotProduct[margin:-margin,margin:-margin]
     
     dotProduct_arr = np.zeros((TIME_MAX + 1, 960, 960))
-
     for time in range(1, TIME_MAX):
         cmlFlow = cmlFlow_arr[time]
         dotProduct_arr[time] = dot_product(cmlFlow, windowSize, flowThreshold)
-
     return dotProduct_arr
 
 def output_dot_video(dotProduct_arr, dotProductThreshold, fixDirection_arr, videoFilepath):
@@ -89,12 +85,14 @@ def output_dot_video(dotProduct_arr, dotProductThreshold, fixDirection_arr, vide
         img = ciputil.get_image(time=time, page=PAGE)
         stabImg = ciputil.get_stabilized_image(img, fixDirection_arr[PAGE][time])
 
-        reversePoint_arr = np.where(dotProduct_arr[time] < dotProductThreshold)
+        divisionPoint_arr = np.where(dotProduct_arr[time] < dotProductThreshold)
 
         dotImg = stabImg
-        for i in range(len(reversePoint_arr[0])):
-            x = reversePoint_arr[1][i]
-            y = reversePoint_arr[0][i]
+        assert divisionPoint_arr.shape[0] == 2
+        numPoints = divisionPoint_arr.shape[1]
+        for i in range(numPoints):
+            x = divisionPoint_arr[1][i]
+            y = divisionPoint_arr[0][i]
             dotImg = cv2.circle(dotImg, (x, y), 3, (255,0,0), -1)
         video.write(dotImg)
     video.release()
@@ -110,17 +108,16 @@ def main():
     PAGE, windowSize, cmlFlowFilepath, videoFilepath = ciputil.read_config_cumulative(configFilepath)
     cmlFlow_arr = np.load(cmlFlowFilepath)
 
-    windowSize, flowThreshold, dotThreshold, calcDotFlag, dumpFilepath, videoFilepath = ciputil.read_config_dot(configFilepath)
+    windowSize, flowThreshold, dotThreshold, recalculate, dumpFilepath, videoFilepath = ciputil.read_config_dot(configFilepath)
 
-    if calcDotFlag:
+    if recalculate:
         print("START: calculating dot product")
         dotProduct_arr = calc_dot_product(cmlFlow_arr, windowSize, flowThreshold)
         np.save(dumpFilepath, dotProduct_arr)
         print("DONE: dump to {}".format(dumpFilepath))
     else:
-        print("START: load dot product array from {}".format(dumpFilepath))
         dotProduct_arr = np.load(dumpFilepath)
-        print("DONE: load dot product array")
+        print("DONE: load dot product array from {}".format(dumpFilepath))
 
     if OUTPUT_VIDEO:
         fixDirectionFilepath=ciputil.read_config_fixDirection(configFilepath)
