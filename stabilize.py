41a7688b10eae536c32dc86330faa80e7c50bf7a
kenya-sk <kenya0sakka@gmail.com>
2017-09-27 22:46:28 +0900

    Issue47: modify

--- stabilize.py
+++ stabilize.py
@@ -85,10 +85,9 @@ def calc_fix_direction(angleThresh):
             prevGray = cv2.cvtColor(prevImg, cv2.COLOR_BGR2GRAY)
             nextImg = ciputil.get_image(time=time, page=page)
             flowMask = get_binarization(prevGray)
-            prevFeature = cv2.goodFeaturesToTrack(prevGray, mask=flowMask, **feature_params)
+            prevFeature = cv2.goodFegit aturesToTrack(prevGray, mask=flowMask, **feature_params)
             try:
                 prevFeatureFiltered, nextFeatureFiltered = get_feature(prevImg, nextImg, prevFeature)
-                #print("page:{0:03d} time:{1:03d} {2}".format(page, time, prevFeatureFiltered.shape[0]))
                 if prevFeatureFiltered.shape[0] <= 50:
                     raise FeatureError("Not detect feature")
                 sparseFlow = calc_sparseFlow(prevFeatureFiltered, nextFeatureFiltered)
