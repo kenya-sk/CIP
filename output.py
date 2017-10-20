3fecfd884355361b2980e4bb92547707a234caa8
takebiolac <takefumi.biology@gmail.com>
2017-10-20 16:38:44 +0900

    adjust parameters

--- output.py
+++ output.py
@@ -13,7 +13,8 @@ def get_df(dotThreshold):
     print("START: load dot")
     
     dct_lst=[]
-    for page in range(1, PAGE_MAX+1):
+    for page in range(1,PAGE_MAX+1):
+        dctPage_lst=[]
         dotFilepath="./out/dot_{}.npy".format(page)
         dotProduct_arr = np.load(dotFilepath)
         for time in range(1, TIME_MAX+1):
@@ -25,7 +26,10 @@ def get_df(dotThreshold):
                 dct["y"]=y
                 dct["time"]=time
                 dct["page"]=page
-                dct_lst.append(dct)
+                dctPage_lst.append(dct)
+        if len(dctPage_lst) > 10000:
+            dctPage_lst = np.random.choice(dctPage_lst, 10000)
+        dct_lst.extend(dctPage_lst)
     df=pd.DataFrame(dct_lst)
     return df
 
@@ -34,11 +38,11 @@ def classify(df):
     
     norm_df=pd.DataFrame(index=df.index)
     norm_df["time"]=df["time"]/TIME_MAX/5
-    norm_df["page"]=df["page"]/PAGE_MAX/5
+    norm_df["page"]=df["page"]/PAGE_MAX/10
     norm_df["x"]=df["x"]/960
     norm_df["y"]=df["y"]/960
     
-    dbscan = DBSCAN(eps=0.02,min_samples=1000).fit(norm_df)
+    dbscan = DBSCAN(eps=0.02,min_samples=100).fit(norm_df)
     df["label"]=dbscan.labels_
     unique, counts = np.unique(df["label"], return_counts=True)
     print(dict(zip(unique, counts)))
@@ -50,11 +54,20 @@ def output(df, fixDirection_arr, outputFilepath):
     cellWidth=50
     
     with open(outputFilepath, "w") as f:
+        divisionEvent_lst=[]
+        for label in range(numDetect):
+            filter_df=df[df["label"]==label]
+            tf=np.min(filter_df["time"]) #time first
+            tl=np.max(filter_df["time"])
+            if tl - tf > 10:
+                divisionEvent_lst.append(label)
+        numDetect = len(divisionEvent_lst)
+        
         f.write("{}\n".format(TIME_MAX))
         f.write("{}\n".format(numDetect))
         f.write("\n")
 
-        for label in range(numDetect):
+        for label in divisionEvent_lst:
             filter_df=df[df["label"]==label]
             mx=np.mean(filter_df["x"])
             my=np.mean(filter_df["y"])
@@ -64,12 +77,12 @@ def output(df, fixDirection_arr, outputFilepath):
 
             for time in range(1, TIME_MAX+1):
                 if tf<=time<tl:
-                    ax=int(my-cellWidth/2+fixDirection_arr[int(mz)][time][0]-240)
-                    ay=int(mx-cellWidth/2+fixDirection_arr[int(mz)][time][1]-240)
-                    az=int(mz - cellHeight/2)
-                    bx=int(my+cellWidth/2+fixDirection_arr[int(mz)][time][0]-240)
-                    by=int(mx+cellWidth/2+fixDirection_arr[int(mz)][time][1]-240)
-                    bz=int(mz + cellHeight/2)
+                    ax=max(0, int(my-cellWidth/2+fixDirection_arr[int(mz)][time][0]-240))
+                    ay=max(0, int(mx-cellWidth/2+fixDirection_arr[int(mz)][time][1]-240))
+                    az=max(0, int(mz - cellHeight/2))
+                    bx=min(480, int(my+cellWidth/2+fixDirection_arr[int(mz)][time][0]-240))
+                    by=min(480, int(mx+cellWidth/2+fixDirection_arr[int(mz)][time][1]-240))
+                    bz=min(PAGE_MAX, int(mz + cellHeight/2))
                 else:
                     ax,ay,az,bx,by,bz=-1,-1,-1,-1,-1,-1
                 f.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(ax,ay,az,bx,by,bz))
@@ -84,6 +97,7 @@ def main():
     #_, _, _ , dotThreshold, _, _ = ciputil.read_config_dot(configFilepath)
     dotThreshold=-10
     df=get_df(dotThreshold)
+    df=df[::10]
     df=classify(df)
     
     _, fixDirectionFilepath, _ = ciputil.read_config_stabilize(configFilepath)
